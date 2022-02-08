f1c56f04b7e7831f37f3eb2e90afef0853d1fda9
joonsung <wnstjd343442@gmail.com>
2022-02-08 21:55:34 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+const { User } = require("./models/User");
+const config = require("./config/key");
+const bodyParser = require("body-parser");
+const { json } = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 mongoose
-  .connect(
-    "mongodb+srv://wnstjd:1234qwer@boilerplate.i1isq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      //   useCreatendex: true,
-      //  useFindAndModify: false,
-      //  useNewUrlParser: true,
-      //  useUnifiedTopology: true,
-    }
-  )
+  .connect(config.mongoURL, {
+    //   useCreatendex: true,
+    //  useFindAndModify: false,
+    //  useNewUrlParser: true,
+    //  useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("DB connected");
   })
@@ -20,7 +24,22 @@ mongoose
     console.log(err);
   });
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World");
+});
+
+app.post("/register", (req, res) => {
+  // 회원 가입 정보들을 데이터 베이스에 넣어주는 코드
+
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) {
+      return res.json({ success: false, err });
+    } else {
+      return res.status(200).json({
+        success: true,
+      });
+    }
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
